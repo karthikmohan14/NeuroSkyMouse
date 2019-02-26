a82a1eacee77227c7d4385e8e824b8bc915c00dd
karthik <karthikmkochi@gmail.com>
2019-02-26 10:31:00 +0530

    pseudo code mod for calibration

---
+++ model.py
@@ -0,0 +1,6 @@
+# generate models for names 
+# get input as name and activty
+# open csv file correspodingly "__name__activity.csv"
+# trim the first and last 10 data entries
+# create min, max heap based on attention, pop out 20 entries use mean squared error for plane based on x,att,med
+# save min_att,max_att,activity to model csv
